64b15188931d6ccd7b6d564797981dd729d00ef1
stoneberg <stoneberg73@gmail.com>
2023-10-16 14:13:24 +0900

    first init

---
+++ centero-api/src/main/java/kr/centero/ghg/api/CenteroApiApp.java
@@ -0,0 +1,11 @@
+package kr.centero.ghg.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenteroApiApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CenteroApiApp.class, args);
+    }
+}
---
+++ centero-core/src/main/java/kr/centero/core/CenteroCoreApp.java
@@ -0,0 +1,11 @@
+package kr.centero.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CenteroCoreApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CenteroCoreApp.class, args);
+    }
+}
