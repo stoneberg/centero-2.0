ef75af9453176a6de121a7423658fa855b607d45
stoneberg <stoneberg73@gmail.com>
2023-10-16 18:11:46 +0900

    core-common example

--- centero-common/src/main/java/kr/centero/common/api/user/service/BoardService.java
+++ centero-common/src/main/java/kr/centero/common/api/user/service/BoardService.java
@@ -4,22 +4,25 @@ import kr.centero.common.api.user.domain.dto.BoardDto;
 import kr.centero.common.api.user.domain.model.Board;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Service
 public class BoardService {
 
     private static final Map<String, Board> boards = new HashMap<>();
-
     static {
         // 게시글 데이터 생성
-        boards.put("1", new Board("1", "제목1", "내용1", "작성자1", "2023-01-01", "2023-01-02"));
-        boards.put("2", new Board("2", "제목2", "내용2", "작성자2", "2023-01-03", "2023-01-04"));
-        boards.put("3", new Board("3", "제목3", "내용3", "작성자3", "2023-01-05", "2023-01-06"));
-        boards.put("4", new Board("4", "제목4", "내용4", "작성자4", "2023-01-07", "2023-01-08"));
-        boards.put("5", new Board("5", "제목5", "내용5", "작성자5", "2023-01-09", "2023-01-10"));
+        boards.put("1", new Board(UUID.randomUUID().toString(), "제목1", "내용1", "작성자1", "2023-01-01", "2023-01-02"));
+        boards.put("2", new Board(UUID.randomUUID().toString(), "제목2", "내용2", "작성자2", "2023-01-03", "2023-01-04"));
+        boards.put("3", new Board(UUID.randomUUID().toString(), "제목3", "내용3", "작성자3", "2023-01-05", "2023-01-06"));
+        boards.put("4", new Board(UUID.randomUUID().toString(), "제목4", "내용4", "작성자4", "2023-01-07", "2023-01-08"));
+        boards.put("5", new Board(UUID.randomUUID().toString(), "제목5", "내용5", "작성자5", "2023-01-09", "2023-01-10"));
+        boards.put("6", new Board(UUID.randomUUID().toString(), "제목6", "내용6", "작성자6", "2023-01-11", "2023-01-12"));
+        boards.put("7", new Board(UUID.randomUUID().toString(), "제목7", "내용7", "작성자7", "2023-01-13", "2023-01-14"));
+        boards.put("8", new Board(UUID.randomUUID().toString(), "제목8", "내용8", "작성자8", "2023-01-15", "2023-01-16"));
+        boards.put("9", new Board(UUID.randomUUID().toString(), "제목9", "내용9", "작성자9", "2023-01-17", "2023-01-18"));
+        boards.put("10", new Board(UUID.randomUUID().toString(), "제목10", "내용10", "작성자10", "2023-01-19", "2023-01-20"));
+        boards.put("11", new Board(UUID.randomUUID().toString(), "제목11", "내용11", "작성자11", "2023-01-21", "2023-01-22"));
     }
 
     public BoardDto.BoardContentResponse findBoardContentById(String id) {
@@ -27,4 +30,22 @@ public class BoardService {
         return BoardDto.BoardContentResponse.from(board);
     }
 
+    public List<BoardDto.BoardContentResponse> findBoardContents() {
+        return boards.values().stream().map(BoardDto.BoardContentResponse::from).toList();
+    }
+
+    public BoardDto.BoardContentResponse addBoardContent(BoardDto.BoardContentRequest boardContentRequest) {
+        Board board = Board.builder()
+                .id(UUID.randomUUID().toString())
+                .title(boardContentRequest.getTitle())
+                .content(boardContentRequest.getContent())
+                .writer(boardContentRequest.getWriter())
+                .regDate(new Date().toString())
+                .modDate(new Date().toString())
+                .build();
+
+        boards.put(board.getId(), board);
+
+        return BoardDto.BoardContentResponse.from(board);
+    }
 }
--- centero-common/src/main/java/kr/centero/common/api/user/web/BoardController.java
+++ centero-common/src/main/java/kr/centero/common/api/user/web/BoardController.java
@@ -1,13 +1,11 @@
 package kr.centero.common.api.user.web;
 
+import kr.centero.common.api.user.domain.dto.BoardDto;
 import kr.centero.common.api.user.service.BoardService;
 import kr.centero.core.payload.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/boards")
@@ -17,8 +15,18 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse> getBoard(@PathVariable("id") String id) {
+    public ResponseEntity<ApiResponse> findBoardContentById(@PathVariable("id") String id) {
         return ApiResponse.ok(boardService.findBoardContentById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse> findBoardContents() {
+        return ApiResponse.ok(boardService.findBoardContents());
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse> addBoardContent(@RequestBody BoardDto.BoardContentRequest boardCreateRequest) {
+        return ApiResponse.ok(boardService.addBoardContent(boardCreateRequest));
     }
 
 }
